2eb0a833815f00c7162c761044a7f8c310cf8a79
Cormac Cureton <cormaccureton@gmail.com>
2021-01-16 22:37:34 -0500

    Main page initial

--- frontend/src/pages/main.js
+++ frontend/src/pages/main.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "./pages.css";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import AuthButton from "../components/authButton";
 
 export default function Home() {
   return (
-    <div className="main">
-      <h>Main Page</h>
+    <div className="main" style={{ backgroundImage: `url('mainWaves.svg')` }}>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Mood On Repeat</h1>
+            <p>
+              How are you feeling? According to your{" "}
+              <span className="highlight">Spotify</span>
+            </p>
+            {/* TODO Make Spotify green */}
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <AuthButton />
+          </Col>
+        </Row>
+        {/* <Image src="mainWaves.svg" alt="Gradient Waves"></Image> */}
+      </Container>
     </div>
   );
 }
