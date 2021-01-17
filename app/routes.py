1efd71b4a5f61706ca27a7c688b88dd9fbe43c08
Dana Zarezankova <dzarezan@uwaterloo.ca>
2021-01-16 22:45:51 -0500

    Resolving conflicts

--- app/routes.py
+++ app/routes.py
@@ -2,29 +2,34 @@ from flask import Flask, redirect, render_template
 from app import app
 import urllib
 from urllib.parse import quote
+import json
+import base64
+import requests
+from flask import request
 
 # Import spotify cilent IDs and secrets
-
 CLIENT_ID = "2f3c4641f2b04770842408e01d627b74"
 CLIENT_SECRET = "dc1797eeaa5045f3814ddd362d529c9c"
 
-spotify_auth_url = "https://accounts.spotify.com/authorize"
-spotify_token_url = "https://accounts.spotify.com/api/token"
-spotify_api_base_url = "https://api.spotify.com"
+# Spotify URLS
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
 
-# Import urls for our app 
-
-client_side_url = "http://localhost:3000/"
-redirect_uri = "http://localhost:3000/spotify_sentiment_analysis"
-scope = 'user-read-private user-read-playback-state user-modify-playback-state user-library-read'
-state = ""
-show_dialogue_bool = True
-SHOW_DIALOG_str = str(show_dialogue_bool).lower()
+# Server-side Parameters
+CLIENT_SIDE_URL = "http://127.0.0.1:5000/"
+REDIRECT_URI = "http://127.0.0.1:5000/spotify_sentiment_analysis"
+SCOPE = 'user-read-private user-read-playback-state user-modify-playback-state user-library-read'
+STATE = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
 
 auth_query_parameters = {
     "response_type": "code",
-    "redirect_uri": redirect_uri,
-    "scope": scope,
+    "redirect_uri": REDIRECT_URI,
+    "scope": SCOPE,
     "client_id": CLIENT_ID
 }
 
@@ -35,11 +40,37 @@ def spotify():
 @app.route('/spotify_authentication')
 def spotify_auth():
     url_args = "&".join(["{}={}".format(key,urllib.parse.quote(val)) for key, val in auth_query_parameters.items()])
-    auth_url = "{}/?{}".format(spotify_auth_url, url_args)
+    auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
     return redirect(auth_url)
  
 @app.route('/spotify_sentiment_analysis')
 def spotify_sentiment_analysis():
+    auth_token = request.args['code']
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": REDIRECT_URI
+    }
+    
+    base64encoded = base64.b64encode("{}:{}".format(CLIENT_ID, CLIENT_SECRET).encode())
+    headers = {"Authorization": "Basic {}".format(base64encoded.decode())}
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload, headers=headers)
+
+    response_data = json.loads(post_request.text)
+    access_token = response_data["access_token"]
+    refresh_token = response_data["refresh_token"]
+    token_type = response_data["token_type"]
+    expires_in = response_data["expires_in"]
+
+    authorization_header = {"Authorization":"Bearer {}".format(access_token)}
+
+    # Get profile data
+    user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
+    profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
+    profile_data = json.loads(profile_response.text)
+    
+    print(profile_data)
+
     return render_template('spotify_sentiment_analysis.html')
 
 
